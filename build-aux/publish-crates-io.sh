4b4483773951129514b8bc0bfcff0bec5fe33df4
Sophie Herold <sophie@hemio.de>
2025-01-03 18:18:15 +0100

    CI: Fix crate published detection

--- build-aux/publish-crates-io.sh
+++ build-aux/publish-crates-io.sh
@@ -3,16 +3,16 @@
 # Publish crates to crates.io
 
 function release_crate () {
-    LOCAL_VERSION=$(cargo info "$1" | grep ^version:)
-    LOCAL_VERSION=${LOCAL_VERSION#version: }
-    LOCAL_VERSION=${LOCAL_VERSION% (*}
-    PUBLISHED_VERSION=$(cd /tmp/; cargo info "$1@${LOCAL_VERSION}")
+    LOCAL_VERSION="$(cargo info "$1" | grep ^version:)"
+    LOCAL_VERSION="${LOCAL_VERSION#version: }"
+    LOCAL_VERSION="${LOCAL_VERSION% (*}"
+    PUBLISHED_STATUS_CODE="$(curl -s -w "%{http_code}" -o /dev/null https://crates.io/api/v1/crates/glycin/2.1.0-alpha)"
 
-    if [ $? -ne 0 ]; then
+    if [ "${PUBLISHED_STATUS_CODE}" -eq "404" ]; then
         echo "Publishing '$1' with version '${LOCAL_VERSION}'."
-        cargo publish -p $1 2>&1
+        cargo publish -p "$1"
     else
-        echo "Crate '$1' with version '${LOCAL_VERSION}' already published. Skipping."
+        echo "Crate '$1' with version '${LOCAL_VERSION}' already published (status code ${PUBLISHED_STATUS_CODE}). Skipping."
     fi
 }
 
