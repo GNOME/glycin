4db2d067ff2eb41c7f1763f241f66dfb4ca545a2
Sophie Herold <sophie@hemio.de>
2025-03-27 21:26:09 +0100

    image-rs/editing: Fix handling of already rotated
    Previously, existing exif orientations were just ignored.
    
    Issue GNOME/loupe#481

--- loaders/glycin-image-rs/src/editor/png.rs
+++ loaders/glycin-image-rs/src/editor/png.rs
@@ -1,12 +1,13 @@
 use std::io::{Cursor, Read};
 
+use glycin_utils::operations::Operations;
 use glycin_utils::*;
 use image::{ImageDecoder, ImageEncoder};
 use image_rs::Handler;
 
 pub fn apply(
     mut stream: glycin_utils::UnixStream,
-    operations: glycin_utils::operations::Operations,
+    mut operations: glycin_utils::operations::Operations,
 ) -> Result<CompleteEditorOutput, glycin_utils::ProcessError> {
     let mut old_png_data: Vec<u8> = Vec::new();
     stream.read_to_end(&mut old_png_data).internal_error()?;
@@ -17,6 +18,12 @@ pub fn apply(
     let mut simple_frame = Handler::default().simple_frame(&decoder).expected_error()?;
     let mut buf = vec![0; decoder.total_bytes() as usize];
     decoder.read_image(&mut buf).expected_error()?;
+
+    let mut old_png = gufo::png::Png::new(old_png_data).expected_error()?;
+    let metadata = gufo::Metadata::for_png(&old_png);
+    if let Some(orientation) = metadata.orientation() {
+        operations.prepend(Operations::new_orientation(orientation));
+    }
 
     buf = operations.apply(buf, &mut simple_frame).expected_error()?;
 
@@ -41,13 +48,57 @@ pub fn apply(
         .internal_error()?;
 
     let new_png = gufo::png::Png::new(new_png_data.into_inner()).expected_error()?;
-    let mut old_png = gufo::png::Png::new(old_png_data).expected_error()?;
 
     // Keep old PNG with its metadata but replace image data with the one from new
     // one
     old_png.replace_image_data(&new_png).expected_error()?;
 
-    let data = BinaryData::from_data(&old_png.into_inner()).expected_error()?;
+    let raw_data = reset_exif_orientation(old_png);
+
+    let data = BinaryData::from_data(&raw_data).expected_error()?;
 
     Ok(CompleteEditorOutput::new(data))
+}
+
+fn reset_exif_orientation(mut png: gufo::png::Png) -> Vec<u8> {
+    let ornt = png
+        .chunks()
+        .into_iter()
+        .find(|x| x.chunk_type().bytes() == *b"orNT");
+
+    if let Some(ornt) = ornt {
+        let _ = gufo::png::remove_chunk!(png, ornt);
+    }
+
+    let mut byte_pos = Vec::new();
+    for chunk in png.chunks() {
+        if matches!(chunk.chunk_type(), gufo::png::ChunkType::eXIf) {
+            let exif_data = chunk.chunk_data().to_vec();
+            if let Some(tag_position) = exif_orientation_value_position(exif_data) {
+                let chunk_position = chunk.unsafe_raw_chunk().complete_data().start as u64;
+                byte_pos.push(chunk_position + 8 + tag_position as u64);
+            }
+        }
+    }
+
+    let byte_changes = ByteChanges::from_slice(
+        &byte_pos
+            .into_iter()
+            .map(|x| (x, gufo_common::orientation::Orientation::Id as u8))
+            .collect::<Vec<_>>(),
+    );
+
+    let mut png_data = png.into_inner();
+    byte_changes.apply(&mut png_data);
+    png_data
+}
+
+fn exif_orientation_value_position(data: Vec<u8>) -> Option<usize> {
+    let mut exif = gufo_exif::internal::ExifRaw::new(data);
+    exif.decode().ok()?;
+    if let Some(entry) = exif.lookup_entry(gufo_common::field::Orientation) {
+        Some(entry.value_offset_position() as usize)
+    } else {
+        None
+    }
 }
