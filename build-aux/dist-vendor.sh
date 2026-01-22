5489260ed21b26af1c92d61b67c16f499ed9afc4
Sophie Herold <sophie@hemio.de>
2026-01-22 19:15:37 +0100

    meson: Do not include rebind in release
    Part-of: <https://gitlab.gnome.org/GNOME/glycin/-/merge_requests/350>

--- build-aux/dist-vendor.sh
+++ build-aux/dist-vendor.sh
@@ -6,23 +6,28 @@ cd "$MESON_PROJECT_DIST_ROOT"
 VERSION="$($MESON_PROJECT_SOURCE_ROOT/build-aux/crates-version.py glycin cargo)"
 
 # Remove crates.io packaged part
-sed -i 's/"glycin",\?//' Cargo.toml
+sed -i 's/\s\{4\}"glycin",\?//' Cargo.toml
 rm -r glycin
 awk -i inplace -v RS= -v ORS='\n\n' '!/name = "glycin"/' Cargo.lock
 
-sed -i 's/"glycin-utils",\?//' Cargo.toml
+sed -i 's/\s\{4\}"glycin-utils",\?//' Cargo.toml
 rm -r glycin-utils
 awk -i inplace -v RS= -v ORS='\n\n' '!/name = "glycin-utils"/' Cargo.lock
 
-sed -i 's/"glycin-common",\?//' Cargo.toml
+sed -i 's/\s\{4\}"glycin-common",\?//' Cargo.toml
 rm -r glycin-common
 awk -i inplace -v RS= -v ORS='\n\n' '!/name = "glycin-common"/' Cargo.lock
 
-echo "Showing changed Cargo.toml:"
-cat Cargo.toml
+sed -i 's/\s\{4\}"libglycin-[^"]*",\?//' Cargo.toml
+rm -r libglycin-rebind
+awk -i inplace -v RS= -v ORS='\n\n' '!/name = "libglycin-rebind"/' Cargo.lock
 
 sed -i "s/, path = \"glycin-common\/\"//g" Cargo.toml
 sed -i "s/, path = \"glycin-utils\/\"//g" Cargo.toml
+sed -i "s/, path = \"libglycin-rebind\/libglycin-rebind\/\"//g" Cargo.toml
 sed -i "s/path = \"glycin\/\"/version = \"$VERSION\"/g" Cargo.toml
+
+echo "Showing changed Cargo.toml:"
+cat Cargo.toml
 
 cargo check -p tests
