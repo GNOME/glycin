31f942a339cd31af46748172f58003bf8998fe04
Sophie Herold <sophie@hemio.de>
2026-02-01 20:20:36 +0100

    glycin/sandbox: Disable sandbox on uid map error
    Disable sandbox when bwrap fails with "bwrap: setting up uid map:
    Permission denied".

--- glycin/src/sandbox.rs
+++ glycin/src/sandbox.rs
@@ -745,6 +745,7 @@ impl Sandbox {
                     "No permissions to creating new namespace",
                     // Wording of an old Debian patch
                     "No permissions to create new namespace",
+                    "bwrap: setting up uid map: Permission denied",
                 ]
                 .iter()
                 .any(|y| x.contains(y))
