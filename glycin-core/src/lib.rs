36ddce91622421ca861b5857b3587f058d43cf81
Sophie Herold <sophie@hemio.de>
2026-08-04 14:22:43 +0200

    glycin: Extend documentation

--- glycin-core/src/lib.rs
+++ glycin-core/src/lib.rs
@@ -1,56 +1,10 @@
-//! Glycin allows to decode images into [`gdk::Texture`]s and to extract image
-//! metadata. The decoding happens in sandboxed modular image loaders that have
-//! to be provided as binaries. The [`glycin-utils`](glycin_utils) for more
-//! details.
+//! The `glycin-core` crate contains the implementation for the
+//! [`glycin`](glycin) crate, which you usually want to use. The
+//! [`glycin`](glycin) crate contains the detailled documentation and
+//! automatically selects the right compilation method for the target OS.
 //!
-//! # Example
-//!
-//! You need to enable the `gdk4` feature for this example to work.
-//!
-//! ```no_run
-//! # use glycin_core::*;
-//! # async {
-//! let file = gio::File::for_path("image.jpg");
-//! let image = Loader::new(file).load().await?;
-//!
-//! let height = image.details().height();
-//! let texture = image.next_frame().await?.texture();
-//! # Ok::<(), ErrorCtx>(()) };
-//! ```
-//!
-//! You can pass the [`texture`](Frame#structfield.texture) of a [`Frame`] to
-//! [`gtk4::Image::from_paintable()`] to display the image.
-//!
-//! # External Dependencies
-//!
-//! Glycin requires the libraries *libglib2.0*, *libfontconfig*, and
-//! *libseccomp* packages to be installed. For the `gdk4` feature, *libgtk-4* is
-//! required as well. To actually work with images, loaders for the respective
-//! formats have to be installed. Glycin provides [loaders] for many formats
-//! that are packaged with many distributions. When working in the default
-//! sandbox mode, the `bwrap` binary of *bubblewrap* is required as well. The
-//! required dependencies can usually be installed through commands like
-//!
-//! ```sh
-//! $ apt install libgtk-4-dev libfontconfig-dev libseccomp-dev glycin-loaders bubblewrap
-//! ```
-//!
-//! on Debian/Ubuntu or
-//!
-//! ```sh
-//! $ dnf install gtk4-devel fontconfig-devel libseccomp-devel glycin-loaders bubblewrap
-//! ```
-//!
-//! on Fedora.
-//!
-//! # Features
-//!
-//! - `gdk4` --- Enables interoperability with [`gdk4`](gdk) by enabling to get
-//!   a [`gdk::Texture`] directly.
-//! - `tokio` --- Makes glycin compatible with [`zbus`] using [`tokio`].
-//!
-//! [`gtk4::Image::from_paintable()`]: https://gtk-rs.org/gtk4-rs/git/docs/gtk4/struct.Image.html#method.from_paintable
-//! [loaders]: https://gitlab.gnome.org/GNOME/glycin#supported-image-formats
+//! You can use this crate if you want to explicitly combine builtin and
+//! external loaders.
 
 #![cfg_attr(
     feature = "builtin",
